48a235ea20a6f112846f831d5329cb3b6f19b2b5
yatima99 <tanigawayusuke496@gmail.com>
2024-10-09 11:38:04 +0900

    add view, contoller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
+use App\Http\Controllers\UtilityController;
+use App\Http\Controllers\GameController;
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/helloworld', fn() => view('helloworld'));
+Route::get('/hello', fn() => view('hello', [
+    'name' => '山田',
+    'course' => 'Laravel'
+]));
+
+Route::get('/', fn() => view('index'));
+Route::get('/curriculum', fn() => view('curriculum'));
+
+// 世界の時間
+Route::get('/world-time', [UtilityController::class, 'worldTime'] );
+
+// おみくじ
+Route::get('/omikuji',  [GameController::class, 'omikuji'] );
+
+// モンティ・ホール問題
+Route::get('/monty-hall',  [GameController::class, 'montyHall']);
